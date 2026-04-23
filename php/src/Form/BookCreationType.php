9d73c3e3f7d64fba3f6bddb94823ad3d78a376e6
MSKTArcanine <marcantoine1999@live.fr>
2026-04-23 22:57:47 +0200

    :sparkles: DOCKER-3 Add BookCreation form type

---
+++ php/src/Form/BookCreationType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookCreationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'rows' => 3,
+                ],
+            ])
+            ->add('summary', TextareaType::class, [
+                'label' => 'Summary',
+                'attr' => [
+                    'rows' => 5,
+                ],
+            ])
+            ->add('pages', IntegerType::class, [
+                'label' => 'Pages',
+            ])
+            ->add('isAvailable', CheckboxType::class, [
+                'label' => 'Available for checkout',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
